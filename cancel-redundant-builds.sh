e50dfd0bcf1096b055ec41ead71ec50b80ea0f62
Alexander Scott <xander@cadcoder.com>
2018-02-13 20:05:04 +1000

    Tests

--- cancel-redundant-builds.sh
+++ cancel-redundant-builds.sh
@@ -3,8 +3,7 @@
 if [[ -n "$CIRCLECI" ]];then
 	BASE_API_URL="https://circleci.com/api/v1.1/project/github/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH"
 else
-    CIRCLE_BUILD_NUM="8720"
-	BASE_API_URL="http://localhost:3000/test.json"
+	BASE_API_URL="file"
 	CIRCLE_TOKEN="UNDEFINED"
 fi
 
@@ -26,21 +25,28 @@ require_var () {
 
 circle_get () {
 	require_var CIRCLE_TOKEN
-	curl --silent \
-        -H "Content-Type: application/json" \
-		${BASE_API_URL}
+	if [[ "$BASE_API_URL" == "file" ]]; then
+	    cat test.json
+    else
+        curl --silent \
+            -H "Content-Type: application/json" \
+            ${BASE_API_URL}
+    fi
 }
 
 v=$(circle_get)
+require_var v
 current_build=$(echo ${v} | jq --raw-output ".[] | select(.build_num==$CIRCLE_BUILD_NUM)")
+require_var current_build
 current_workflow_id=$(echo ${current_build} | jq --raw-output ".workflows.workflow_id")
+require_var current_workflow_id
 current_author_date=$(echo ${current_build} | jq --raw-output ".author_date")
+require_var current_author_date
 matches=$(echo ${v} | jq --raw-output ".[] | select((.workflows.workflow_id!=\"$current_workflow_id\") and (.author_date > \"$current_author_date\")) | .workflows.job_id")
 
 if [[ -z "$matches" ]];then
     echo "No future builds found. Progressing"
 else
-    echo "I Should Cancel"
     echo "Future builds found. Halting"
     exit 1
 fi
---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+export CIRCLECI=""
+function test_should_abort(){
+    export CIRCLE_BUILD_NUM="8647"
+    ./cancel-redundant-builds.sh
+    status=$?
+    if [[ ${status} == 0 ]];then
+        echo "Test failed"
+        exit 1
+    fi
+}
+
+function test_should_pass(){
+    export CIRCLE_BUILD_NUM="8720"
+    ./cancel-redundant-builds.sh
+    status=$?
+    if [[ ${status} != 0 ]];then
+        echo "Test failed"
+        exit 1
+    fi
+}
+
+test_should_abort && \
+test_should_pass

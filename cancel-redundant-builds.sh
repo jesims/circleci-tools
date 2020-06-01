23363982aa3cf4b21854852246518d23e82e7244
André de Jager <andrethehunter@gmail.com>
2020-06-01 10:17:37 +1000

    Exit if no previous builds

--- cancel-redundant-builds.sh
+++ cancel-redundant-builds.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_VCS_TYPE=${PROJECT_VCS_TYPE:-'github'}
-if [[ -n "$CIRCLECI" ]];then
+if [ -n "$CIRCLECI" ];then
 	BASE_API_URL="https://circleci.com/api/v1.1/project/$PROJECT_VCS_TYPE/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME/tree/$CIRCLE_BRANCH"
 else
-	BASE_API_URL="file"
+	BASE_API_URL="${BASE_API_URL:-file}"
 	CIRCLE_TOKEN="UNDEFINED"
 fi
 
@@ -13,8 +12,9 @@ echo-error () {
 }
 
 var-set () {
-	local val=$(eval "echo \$$1")
-	test -n "$val"
+	local val
+	val=$(eval "echo \$$1")
+	[ -n "$val" ]
 }
 
 require-var () {
@@ -25,10 +25,12 @@ require-var () {
 }
 
 circle-get () {
-	require-var CIRCLE_TOKEN
-	if [[ "$BASE_API_URL" == "file" ]];then
+	if [ "$BASE_API_URL" = 'file' ];then
 		cat test.json
+	elif [ "$BASE_API_URL" = 'empty' ];then
+		cat empty.json
 	else
+		require-var CIRCLE_TOKEN
 		curl --silent \
 			-H "Content-Type: application/json" \
 			"${BASE_API_URL}?circle-token=${CIRCLE_TOKEN}"
@@ -37,21 +39,25 @@ circle-get () {
 
 node-date() {
 	local date="$1"
-	if [[ -n "$date" ]] && [[ "$date" != "null" ]];then
+	if [ -n "$date" ] && [ "$date" != 'null' ];then
 		node -e "console.log(new Date(\"${date}\"))"
 	fi
 }
 
 v=$(circle-get)
 require-var v
+if [ "$(echo "$v" | jq)" == '[]' ];then
+	#no previous builds found
+	exit 0
+fi
 current_build=$(echo "$v" | jq --raw-output ".[] | select(.build_num==$CIRCLE_BUILD_NUM)")
 require-var current_build
-current_workflow_id=$(echo ${current_build} | jq --raw-output ".workflows.workflow_id")
+current_workflow_id=$(echo "${current_build}" | jq --raw-output ".workflows.workflow_id")
 require-var current_workflow_id
-current_author_date=$(node-date $(echo ${current_build} | jq --raw-output ".author_date"))
-matches=$(echo ${v} | jq --raw-output ".[] | select(.workflows.workflow_id!=\"$current_workflow_id\") | .author_date")
+current_author_date=$(node-date "$(echo "${current_build}" | jq --raw-output ".author_date")")
+matches=$(echo "${v}" | jq --raw-output ".[] | select(.workflows.workflow_id!=\"$current_workflow_id\") | .author_date")
 
-if [[ -n "$current_author_date" ]];then
+if [ -n "$current_author_date" ];then
 	for date in ${matches};do
 		date=$(node-date "$date")
 		if [[ "$date" > "$current_author_date" ]]; then
--- test.sh
+++ test.sh
@@ -1,38 +1,52 @@
 #!/usr/bin/env bash
+cd "$(realpath "$(dirname "$0")")" &&
+source bindle/project.sh
+if [ $? -ne 0 ]; then
+	exit 1
+fi
 
-export CIRCLECI=""
-export PROJECT_VCS_TYPE="github"
+export CIRCLECI=''
 
-function test_should_abort(){
-    export CIRCLE_BUILD_NUM="9305"
-    ./cancel-redundant-builds.sh
-    status=$?
-    if [[ ${status} == 0 ]];then
-        echo "Test failed"
-        exit 1
-    fi
+assert-not-failed(){
+	if [[ $? != 0 ]];then
+		echo "Test failed"
+		exit 1
+	fi
 }
 
-function test_should_pass(){
-    export CIRCLE_BUILD_NUM="9311"
-    ./cancel-redundant-builds.sh
-    status=$?
-    if [[ ${status} != 0 ]];then
-        echo "Test failed"
-        exit 1
-    fi
+assert-failed(){
+	if [[ $? == 0 ]];then
+		echo "Test failed"
+		exit 1
+	fi
 }
 
-function test_should_skip(){
-    export CIRCLE_BUILD_NUM="9312"
-    ./cancel-redundant-builds.sh
-    status=$?
-    if [[ ${status} != 0 ]];then
-        echo "Test failed"
-        exit 1
-    fi
+test_should_abort(){
+		export CIRCLE_BUILD_NUM='9305'
+		./cancel-redundant-builds.sh
+		assert-failed
 }
 
+test_should_pass(){
+	export CIRCLE_BUILD_NUM='9311'
+	./cancel-redundant-builds.sh
+	assert-not-failed
+}
+
+test_should_skip(){
+	export CIRCLE_BUILD_NUM='9312'
+	./cancel-redundant-builds.sh
+	assert-not-failed
+}
+
+test_empty(){
+	export BASE_API_URL='empty'
+	./cancel-redundant-builds.sh
+	assert-not-failed
+}
+
+-lint &&
 test_should_abort && \
 test_should_pass && \
-test_should_skip
+test_should_skip && \
+test_empty
